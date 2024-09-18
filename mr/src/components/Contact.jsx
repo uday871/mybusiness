47e07d2618c86678f603aeea22158c77180a738e
uday871 <uday871@gmail.com>
2024-09-18 20:48:45 +0530

    install update

--- mr/src/components/Contact.jsx
+++ mr/src/components/Contact.jsx
@@ -7,3 +7,5 @@ const Contact = () => {
 }
 
 export default Contact
+
+
--- mr/vite.config.js
+++ mr/vite.config.js
@@ -1,8 +1,31 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
+import { VitePWA } from 'vite-plugin-pwa';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: '/mybusiness/', // Add this line
+  base: '/mybusiness/',
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'gshankar',
+        short_name: 'MR',
+        description: 'Enjoy this app',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'images/talk.webp', 
+            sizes: '192x192',
+            type: 'image/webp'
+          },
+          {
+            src: 'images/icon-512x512.png', 
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ]
 });
