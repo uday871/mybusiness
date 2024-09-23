45316f9ae159bf972f341839de9298e0008c5bda
uday871 <uday871@gmail.com>
2024-09-24 01:07:23 +0530

    Updates

--- assets/mrbr-EEQUnnw9.js
+++
@@ -1,1 +0,0 @@
-const s="/mybusiness/mr/assets/mrbr-CD3Lpvx-.webp";export{s as w};
--- registerSW.js
+++
@@ -1,1 +0,0 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/mybusiness/mr/sw.js', { scope: '/mybusiness/mr/' })})}
