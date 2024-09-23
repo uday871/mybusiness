a23e60c4d0c9fee79f23a145f2b2f5be64b754c8
uday871 <uday871@gmail.com>
2024-09-24 00:55:10 +0530

    changes in vite configuration

--- mr/vite.config.js
+++ mr/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react-swc';
 import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
-  base: '/mybusiness/mr',
+  base: '/mybusiness/mr/',
   plugins: [
     react(),
     VitePWA({
