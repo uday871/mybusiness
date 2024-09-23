d39726352a430733522f10297f84c4f24c440338
uday871 <uday871@gmail.com>
2024-09-24 00:43:05 +0530

    differenciate admin and client

--- admin/vite.config.js
+++ admin/vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/mybusiness/', 
+  base: '/mybusiness/admin', 
   plugins: [react()],
 })
