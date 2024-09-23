f662436cfc5015cbc413b8d957a387b56bac580f
uday871 <uday871@gmail.com>
2024-09-24 00:45:27 +0530

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/mybusiness/sw.js', { scope: '/mybusiness/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/mybusiness/mr/sw.js', { scope: '/mybusiness/mr/' })})}
